4d9294af622c34c3ae5ee2e3adff3aa885b15137
naoualBoutahar <86423971+naoualBoutahar@users.noreply.github.com>
2022-05-27 15:50:30 +0100

    My first File

---
+++ src/main/java/ma/ensao/CatMvc0Application.java
@@ -0,0 +1,13 @@
+package ma.ensao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatMvc0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatMvc0Application.class, args);
+	}
+
+}
---
+++ src/test/java/ma/ensao/CatMvc0ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ensao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatMvc0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
