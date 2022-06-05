054b6a5c18238a3b842166f0be7d7fab240eb7a7
Naoual Boutahar <na.boutahar@gmail.com>
2022-06-05 15:42:31 +0100

    delete fct

--- src/main/java/ma/ensao/web/ProduitController.java
+++ src/main/java/ma/ensao/web/ProduitController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -38,5 +39,10 @@ public class ProduitController {
 		model.addAttribute("pageCourante",p);
 		model.addAttribute("motCle",mc);		
 		return "produits";
+	}
+	@RequestMapping(value="/delete",method=RequestMethod.GET)
+	public String delete(long id,String motCle,int page,int size){
+		produitRepository.deleteById(id);
+		return "redirect:/index?page="+page+"&size="+size+"&motCle="+motCle; 
 	}
 }
