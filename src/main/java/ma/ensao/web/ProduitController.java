6d11572fcafacd02eb676ac07ea5d330ae24a354
Naoual Boutahar <na.boutahar@gmail.com>
2022-06-05 14:02:23 +0100

    pagination

--- src/main/java/ma/ensao/web/ProduitController.java
+++ src/main/java/ma/ensao/web/ProduitController.java
@@ -1,9 +1,12 @@
 package ma.ensao.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import ma.ensao.dao.ProduitRepository;
@@ -21,9 +24,16 @@ public class ProduitController {
 	//quand je tape http://localhost/index ->executer la meth index
 	@RequestMapping(value = "/index")
 	//meth qui retourne le nom d la vue qui sera envoyee
-	public String index(Model model) {
-		List<Produit> produits=produitRepository.findAll();
-		model.addAttribute("listProduits",produits);
+	//@RequestParam demande au dispatcher servlet dutiliser la meth getParam pour trouver parametre  page et laffecter a p
+	public String index(Model model,
+			@RequestParam(name="page",defaultValue="0")int p,
+			@RequestParam(name="size",defaultValue="5")int s){
+		Page <Produit> pageProduits=produitRepository.findAll(PageRequest.of(p,s));
+		model.addAttribute("listProduits",pageProduits.getContent());
+		int[] pages=new int[pageProduits.getTotalPages()];
+		model.addAttribute("pages",pages);
+		model.addAttribute("size",s);
+		model.addAttribute("pageCourante",p);
 		return "produits";
 	}
 }
